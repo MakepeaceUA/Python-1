67b87c67dac13c5af2620c110d76a8b98fffd7d4
Arsen Sevastyanov <130586920+MakepeaceUA@users.noreply.github.com>
2025-10-26 19:29:01 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,59 @@
+#Задание 1
+# num1 = int(input("Enter first number:"))
+# num2 = int(input("Enter second number:"))
+# num3 = int(input("Enter third number:"))
+
+# print("Choice operation:\n1: +\n2: *")
+# choice = input()
+
+# if choice == "1":
+#     res = num1 + num2 + num3
+#     print(f"Sum = {res}")
+# elif choice == "2":
+#     res = num1 * num2 * num3
+#     print(f"Multiplication: = {res}")
+# else:
+#     print("Error")
+
+
+
+#Задание 2
+# num1 = int(input("Enter first number:"))
+# num2 = int(input("Enter second number:"))
+# num3 = int(input("Enter third number:"))
+
+# print("Choice:\n1:Maximum\n2:Minimum\n3:Arithmetic mean")
+
+# choice = input()
+
+# if choice == "1":
+#     print(f"Max: {max(num1,num2,num3)}")
+# elif choice == "2":
+#     print(f"Min: {min(num1,num2,num3)}")
+# elif choice == "3":
+#     print(f"Avg: {(num1 + num2 + num3) / 3}")
+# else:
+#     print("Error")
+
+
+
+#Задание 3
+# meters = float(input("Enter the number of metres: "))
+
+# print("Select a unit for conversion:\n1:Miles\n2:Inches\n3:Yards")
+
+# choice = input()
+
+# if choice == "1":
+#     miles = meters / 1609.34
+#     print(f"{meters} meters = {miles} miles")
+# elif choice == "2":
+#     inches = meters * 39.3701
+#     print(f"{meters} meters = {inches} inches")
+# elif choice == "3":
+#     yards = meters * 1.09361
+#     print(f"{meters} meters = {yards} yards")
+# else:
+#     print("Error")
+
+
